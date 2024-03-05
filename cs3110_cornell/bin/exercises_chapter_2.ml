b9126215952b90c5c4fccbd8deeaeff51ccf8b24
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-03-05 15:05:53 +0100

    Add fast fibonacci recursive. Very cool solution

--- cs3110_cornell/bin/exercises_chapter_2.ml
+++ cs3110_cornell/bin/exercises_chapter_2.ml
@@ -149,3 +149,31 @@ Test your function in the toplevel.
 > let rec fib n = if n <= 1 then n else fib (n-1) + fib (n-2);;
 
 ----- *)
+
+(* Exercise: fib fast [★★★]
+
+How quickly does your implementation of fib compute the 50th Fibonacci number? 
+If it computes nearly instantaneously, congratulations! But the recursive solution most 
+people come up with at first will seem to hang indefinitely. 
+The problem is that the obvious solution computes subproblems repeatedly. 
+For example, computing fib 5 requires computing both fib 3 and fib 4, and if those are 
+computed separately, a lot of work (an exponential amount, in fact) is being redone.
+
+Create a function fib_fast that requires only a linear amount of work. 
+Hint: write a recursive helper function h : int -> int -> int -> int, 
+where h n pp p is defined as follows:
+
+h 1 pp p = p, and
+h n pp p = h (n-1) p (pp+p) for any n > 1.
+
+The idea of h is that it assumes the previous two Fibonacci numbers were pp and p, 
+then computes forward n more numbers. Hence, fib n = h n 0 1 for any n > 0.
+
+What is the first value of n for which fib_fast n is negative, indicating that integer 
+overflow occurred? 
+> let rec h n pp p = if n = 1 then p else h (n-1) p (pp + p)
+> let fib_fast n = h n 0 1
+
+> Very cool solution. n is a counter down, but the fibonacci numbers are 
+computed bottom up. 
+----- *)
